3ba49218a8f28dcdc120d93775395d6fa4628553
Kristian Nikulin <kristiannikulin@mail.ru>
2025-01-12 16:58:24 +0300

    fix: back

--- back/service.js
+++ back/service.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 const app = express();
 const PORT = 5678;
-const AI_TEMP_URL = process.env.AI_TEMP_URL || "http://localhost:1234/ai/temp";
+const AI_TEMP_URL = "http://ai_temp:1234/ai/temp";
 
+app.use(cors());
 app.use(express.json());
 
 app.post("/ai/request", async (req, res) => {
     try {
-        const requestBody = req.body;
-
+        const { query } = req.body;
         const response = await axios.get(AI_TEMP_URL);
+
         res.json(response.data);
     } catch (error) {
         console.error("Ошибка при обработке запроса:", error);
-        res.status(500).json({ error: "Ошибка при обработке запроса" });
+        res.status(500).json({ error });
     }
 });
 
 app.listen(PORT, () => {
-    console.log(`Сервис /ai/request запущен на http://localhost:${PORT}`);
+    console.log(`Сервис /ai/request запущен на ${AI_TEMP_URL}${PORT}`);
 });
